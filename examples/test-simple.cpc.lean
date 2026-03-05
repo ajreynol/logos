7116b498a343f577dbfe183f2ef71a9d589c187c
ajreynol <andrew.j.reynolds@gmail.com>
2026-03-05 12:26:28 -0600

    API and readme, update proofs

--- examples/test-simple.cpc.lean
+++ examples/test-simple.cpc.lean
@@ -7,10 +7,10 @@ def t4 : Term := (Term.Apply t3 t1)
 def t5 : Term := (Term.Apply Term.eq t1)
 def t6 : Term := (Term.Apply t5 t2)
 def t7 : Term := (Term.Apply Term.not t6)
-def s0 : CState := CState.nil
-def s1 : CState := (CState.cons (CStateObj.assume t4) s0)
-def s2 : CState := (CState.cons (CStateObj.assume t7) s1)
-def s3 : CState := (__eo_invoke_cmd s2 (CCmd.step CRule.symm CArgList.nil (CIndexList.cons 0 CIndexList.nil)))
-def s4 : CState := (__eo_invoke_cmd s3 (CCmd.step CRule.contra CArgList.nil (CIndexList.cons 2 (CIndexList.cons 0 CIndexList.nil))))
+def s0 : CState := logos_init_state
+def s1 : CState := (logos_invoke_assume s0 t4)
+def s2 : CState := (logos_invoke_assume s1 t7)
+def s3 : CState := (logos_invoke_cmd s2 (CCmd.step CRule.symm CArgList.nil (CIndexList.cons 0 CIndexList.nil)))
+def s4 : CState := (logos_invoke_cmd s3 (CCmd.step CRule.contra CArgList.nil (CIndexList.cons 2 (CIndexList.cons 0 CIndexList.nil))))
 #eval!
-(__eo_state_is_refutation s4)
+(logos_state_is_refutation s4)
