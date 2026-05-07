92dadb41a1e33544276274227814e13b1d508c39
ajreynol <andrew.j.reynolds@gmail.com>
2026-05-07 12:01:02 -0500

    Another inline

--- CpcMini/Logos.lean
+++ CpcMini/Logos.lean
@@ -464,10 +464,6 @@ def __eo_push_assume_check : Term -> Term -> CState -> CState
   | b, F, s => CState.Stuck
 
 
-def __eo_push_assume : Term -> CState -> CState
-  | F, s => (__eo_push_assume_check (__eo_is_bool_type F) F s)
-
-
 def __eo_push_input_assume_check : Term -> Term -> CState -> CState
   | (Term.Boolean true), F, s => (CState.cons (CStateObj.assume F) s)
   | b, F, s => CState.Stuck
@@ -513,7 +509,7 @@ def __eo_invoke_cmd_step_pop (s : CState) : CState -> CRule -> CArgList -> CInde
 
 def __eo_invoke_cmd : CState -> CCmd -> CState
   | CState.Stuck, c => CState.Stuck
-  | S, (CCmd.assume_push proven) => (__eo_push_assume proven S)
+  | S, (CCmd.assume_push proven) => (__eo_push_assume_check (__eo_is_bool_type proven) proven S)
   | S, (CCmd.check_proven proven) => (__eo_invoke_cmd_check_proven S proven)
   | S, (CCmd.step r args premises) => (__eo_push_proven (__eo_cmd_step_proven S r args premises) S)
   | S, (CCmd.step_pop r args premises) => (__eo_invoke_cmd_step_pop S S r args premises)
