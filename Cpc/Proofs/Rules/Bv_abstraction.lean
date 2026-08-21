ae6b3e1a7b20c3fabba3d5461e743722f3689a2d
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2026-08-21 08:05:25 -0500

    Draft of BV abstraction rule

---
+++ Cpc/Proofs/Rules/Bv_abstraction.lean
@@ -0,0 +1,22 @@
+module
+
+public import Cpc.Proofs.RuleSupport.Support
+import all Cpc.Proofs.RuleSupport.Support
+
+open Eo
+open SmtEval
+open Smtm
+
+set_option linter.unusedVariables false
+set_option maxHeartbeats 10000000
+
+public theorem cmd_step_bv_abstraction_properties
+    (M : SmtModel) (hM : model_total_typed M)
+    (s : CState) (args : CArgList) (premises : CIndexList) :
+  cmdTranslationOk (CCmd.step CRule.bv_abstraction args premises) ->
+  AllHaveBoolType (premiseTermList s premises) ->
+  __eo_typeof (__eo_cmd_step_proven s CRule.bv_abstraction args premises) = Term.Bool ->
+  StepRuleProperties M (premiseTermList s premises)
+    (__eo_cmd_step_proven s CRule.bv_abstraction args premises) :=
+by
+  sorry
