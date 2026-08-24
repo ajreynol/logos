a413b4f6adfd056991bf8c6d031fb8ed7d09b1ef
Andrew Reynolds <andrew.j.reynolds@gmail.com>
2026-08-24 10:59:08 -0500

    Compile simplification

--- Cpc/Logos.lean
+++ Cpc/Logos.lean
@@ -2871,11 +2871,8 @@ def __bv_abstraction_lemma_urem : Term -> Term -> Term -> Term -> Term
 def __bv_abstraction_lemma : Term -> Term
   | Term.Stuck  => Term.Stuck
   | (Term.Apply (Term.Apply (Term.UOp UserOp.imp) (Term.Apply (Term.Apply (Term.UOp UserOp.eq) (Term.Apply (Term.Apply (Term.UOp UserOp.bvmul) x) (Term.Apply (Term.Apply (Term.UOp UserOp.bvmul) s) ns))) t)) l) => (__eo_and (__eo_gt (__bv_bitwidth (__eo_typeof x)) (Term.Numeral 2)) (__eo_and (__eo_eq ns (__eo_to_bin (__bv_bitwidth (__eo_typeof ns)) (Term.Numeral 1))) (__bv_abstraction_lemma_mul x s t l)))
-  | (Term.Apply (Term.Apply (Term.UOp UserOp.imp) (Term.Apply (Term.Apply (Term.UOp UserOp.eq) t) (Term.Apply (Term.Apply (Term.UOp UserOp.bvmul) x) (Term.Apply (Term.Apply (Term.UOp UserOp.bvmul) s) ns)))) l) => (__eo_and (__eo_gt (__bv_bitwidth (__eo_typeof x)) (Term.Numeral 2)) (__eo_and (__eo_eq ns (__eo_to_bin (__bv_bitwidth (__eo_typeof ns)) (Term.Numeral 1))) (__bv_abstraction_lemma_mul x s t l)))
   | (Term.Apply (Term.Apply (Term.UOp UserOp.imp) (Term.Apply (Term.Apply (Term.UOp UserOp.eq) (Term.Apply (Term.Apply (Term.UOp UserOp.bvudiv) x) s)) t)) l) => (__eo_and (__eo_gt (__bv_bitwidth (__eo_typeof x)) (Term.Numeral 2)) (__bv_abstraction_lemma_udiv x s t l))
-  | (Term.Apply (Term.Apply (Term.UOp UserOp.imp) (Term.Apply (Term.Apply (Term.UOp UserOp.eq) t) (Term.Apply (Term.Apply (Term.UOp UserOp.bvudiv) x) s))) l) => (__eo_and (__eo_gt (__bv_bitwidth (__eo_typeof x)) (Term.Numeral 2)) (__bv_abstraction_lemma_udiv x s t l))
   | (Term.Apply (Term.Apply (Term.UOp UserOp.imp) (Term.Apply (Term.Apply (Term.UOp UserOp.eq) (Term.Apply (Term.Apply (Term.UOp UserOp.bvurem) x) s)) t)) l) => (__eo_and (__eo_gt (__bv_bitwidth (__eo_typeof x)) (Term.Numeral 2)) (__bv_abstraction_lemma_urem x s t l))
-  | (Term.Apply (Term.Apply (Term.UOp UserOp.imp) (Term.Apply (Term.Apply (Term.UOp UserOp.eq) t) (Term.Apply (Term.Apply (Term.UOp UserOp.bvurem) x) s))) l) => (__eo_and (__eo_gt (__bv_bitwidth (__eo_typeof x)) (Term.Numeral 2)) (__bv_abstraction_lemma_urem x s t l))
   | l => (Term.Boolean false)
 
 
